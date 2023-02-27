10107477e6aa37608e04d0ae432a9257d6f38956
Aglinskas <aglinska@l001.cm.cluster>
2023-02-27 14:20:47 -0500

    up

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BC-actionpred-seg
 Segmentation stuff
+
+
+
+np.__version__ '1.21.6'
+
+
+pip install omegaconf
+singularity pull yolov8.sif docker://ultralytics/ultralytics
+conda install FFmpeg
+
+https://learnopencv.com/ultralytics-yolov8/#Models-Available-in-YOLOv8
+https://docs.ultralytics.com/tasks/segmentation/
+
+
+https://hub.docker.com/r/ultralytics/ultralytics
---
+++ for_yolo_singularity_make_masks_slurm.py
@@ -0,0 +1,48 @@
+import numpy as np
+from ultralytics import YOLO
+from matplotlib import pyplot as plt
+import os
+import sys
+
+f = sys.argv[-1]
+f = int(f)
+
+model = YOLO("yolov8x-seg.pt")
+#model = YOLO("yolov8n-seg.pt")
+
+indir = '~/BC-actionpred-seg/Data/haa500_v1_1_frames/video/' #ALS_IceBucket_Challenge/ALS_IceBucket_Challenge_009
+indir = os.path.expanduser(indir)
+
+folders = [folder for folder in os.listdir(indir) if not folder.startswith('.')]
+folders.sort()
+#print(folders[0:5])
+
+videos = []
+for folder in folders:
+    conts = os.listdir(os.path.join(indir,folder))
+    conts = [os.path.join(indir,folder,cont) for cont in conts if not cont.startswith('.')]
+    conts.sort()
+    videos.append(conts)
+
+#for f in range(2,len(folders)):
+for v in range(20):
+    frames = [os.path.join(videos[f][v],frame) for frame in os.listdir(videos[f][v]) if frame.endswith('.png')]
+    frames.sort()
+
+    print(videos[f][v])
+
+    for infn in frames:
+
+        try:
+            results = model.predict(infn)
+
+            box = np.array(results[0].boxes.boxes)
+            box.shape
+            np.save(infn.replace('.png','_box.npy'),box)
+
+            arr = np.array(results[0].masks.masks)
+            np.save(infn.replace('.png','_masks.npy'),arr)
+        except:
+            print('passing')
+    print('done')
+
